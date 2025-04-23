a0d8bd32b1492dd0fb47809ea8e353cd396e5a1a
lgl5240 <liangguilin5240@gmail.com>
2025-04-23 17:16:43 -0400

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
+  const [name, setName] = useState("u-plus.ca");
 
   return (
     <>
@@ -54,9 +54,7 @@ function App() {
         >
           Name from API is: {name}
         </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
+         
       </div>
       <p className="read-the-docs">Click on the logos to learn more</p>
     </>
